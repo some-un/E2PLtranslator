346f1692585350c8fdfd590c672b1bacb746951f
some-un <tomasz.kosinski@chalmers.se>
2017-03-13 20:23:35 +0100

    Updating console output for the feature of Issue#6

--- src/mainsrcp/AClassVersion01.java
+++ src/mainsrcp/AClassVersion01.java
@@ -17,7 +17,7 @@ public class AClassVersion01 {
 	    System.out.println(String.format("You said: %s", word));
 	    
 	    // Print the constant word in Pig Latin (v0.1)
-	    System.out.println(String.format("The Pig says: %s", MainProjectClass.constantPigLatinOutputForV01));
+	    System.out.println(String.format("The Pig says: %s", MainProjectClass.constantPigLatinOutputForV01 + MainProjectClass.translationSuffix));
 	    
 	    return MainProjectClass.constantPigLatinOutputForV01 + MainProjectClass.translationSuffix;
 	}
